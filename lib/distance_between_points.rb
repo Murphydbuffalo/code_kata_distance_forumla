bca3747c7b895a41959290dbc592c5c13f94f79d
Dan Murphy <murphydbuffalo@gmail.com>
2014-07-27 21:04:26 -0400

    Tried out sprintf instead of round.

--- lib/distance_between_points.rb
+++ lib/distance_between_points.rb
@@ -5,11 +5,13 @@ def distance(point_A, point_B) # Distance between two points (pairs of X,Y coord
   	  dist = (x_diff + y_diff).round(3) # Rounded to 3 decimal places
     else # If both axis differ, use pythagorean formula:
       dist_squared = (x_diff ** 2) + (y_diff ** 2) # a^2 + b^2 = c^2 
-      dist = Math.sqrt(dist_squared).round(3) # Take square root of c
+      dist = sprintf('%.3f', Math.sqrt(dist_squared)).to_f # Take square root of c
   end
 end
 
 puts distance([1,3], [5,19])
+
+# .sprintf is nice for formatting but it outputs a string, so you need to convert.
 
 
 
